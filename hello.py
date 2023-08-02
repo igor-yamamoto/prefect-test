6c6a931a4e3cf050e313ae85306065c54261c53a
Igor H. C. Yamamotoo <igorhc.yamamoto@gmail>
2023-08-02 16:23:29 -0300

    Subindo arquivos de teste

---
+++ hello.py
@@ -0,0 +1,16 @@
+from prefect import flow, task
+
+name_list = ["Marvin", "Ford", "Arthur"]
+
+@task(log_prints=True)
+def say_hello(name: str):
+    print(f"Hello {name}!")
+
+
+@flow
+def hello_universe(names):
+    for name in names:
+        say_hello(name)
+
+if __name__ == "__main__":
+    hello_universe(names=name_list)
